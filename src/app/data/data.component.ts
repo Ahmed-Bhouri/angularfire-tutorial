c4a58947413c83a000d90709a416f8b51b218a60
Ahmed Bhouri <49922043+Ahmed-Bhouri@users.noreply.github.com>
2025-03-24 23:56:48 +0100

    initial commit

---
+++ src/app/data/data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './data.component.html'
+})
+export class DataComponent {
+
+}
